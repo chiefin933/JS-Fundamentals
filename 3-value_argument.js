77cc1263cc6b22bd0fec45f41b455146a4f8f1e2
Chiefin Paapz <chiefinpaapz@gmail.com>
2025-07-07 15:57:15 +0300

    Create 3-value_argument.js

---
+++ 3-value_argument.js
@@ -0,0 +1,10 @@
+// 2-arguments.js
+const args = process.argv.slice(2);
+
+if (!args[0]) {
+  console.log("No argument");
+} else if (!args[1]) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
